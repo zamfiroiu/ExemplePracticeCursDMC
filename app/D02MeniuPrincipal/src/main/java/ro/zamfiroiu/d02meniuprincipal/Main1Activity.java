31a0917899017a1f99c9efcce8433171124604e4
Alin Zamfiroiu <alinz10@yahoo.com>
2024-03-02 13:41:13 +0200

    Curs 4 - DMC04

---
+++ app/D02MeniuPrincipal/src/main/java/ro/zamfiroiu/d02meniuprincipal/Main1Activity.java
@@ -0,0 +1,14 @@
+package ro.zamfiroiu.d02meniuprincipal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+    }
+}
---
+++ app/D05ScrollView/src/main/java/ro/zamfiroiu/d05scrollview/MainActivity.java
@@ -0,0 +1,14 @@
+package ro.zamfiroiu.d05scrollview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
