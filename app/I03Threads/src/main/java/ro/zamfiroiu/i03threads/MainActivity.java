2dd43a5ea48200f674cea457bd15e41ffd4c44c6
Alin Zamfiroiu <alinz10@yahoo.com>
2024-04-06 12:34:38 +0300

    Threads

---
+++ app/I03Threads/src/main/java/ro/zamfiroiu/i03threads/MainActivity.java
@@ -0,0 +1,57 @@
+package ro.zamfiroiu.i03threads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    private Bitmap bitmap = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Runnable runn=new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection con = null;
+                try{
+                    URL url=new URL("https://static.playtech.ro/wp-content/uploads/2018/03/Cum-cauti-imagini-pe-Google-de-pe-mobil.jpg");
+                    con= (HttpURLConnection) url.openConnection();
+                    if(con.getResponseCode()==HttpURLConnection.HTTP_OK){
+                        InputStream is = con.getInputStream();
+                        bitmap = BitmapFactory.decodeStream(is);
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    if(con!=null){
+                        con.disconnect();
+                    }
+                }
+            }
+        };
+        Thread mythread = new Thread(runn);
+        mythread.start();
+
+
+    }
+
+    public void getTheImage(View view) {
+        ImageView iv=findViewById(R.id.imageView);
+        iv.setImageBitmap(bitmap);
+    }
+}
