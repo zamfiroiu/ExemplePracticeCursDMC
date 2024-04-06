b1fd0dba07e6cd3eb20a574d4fa7184f30255060
Alin Zamfiroiu <alinz10@yahoo.com>
2024-04-06 11:42:14 +0300

    WebView - exemplu simplu

---
+++ app/I01WebView/src/main/java/ro/zamfiroiu/I01WebView/MainActivity.java
@@ -0,0 +1,19 @@
+package ro.zamfiroiu.I01WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView wb=findViewById(R.id.webView);
+        wb.loadUrl("https://www.google.ro/");
+        wb.setWebViewClient(new WebViewClient());
+    }
+}
