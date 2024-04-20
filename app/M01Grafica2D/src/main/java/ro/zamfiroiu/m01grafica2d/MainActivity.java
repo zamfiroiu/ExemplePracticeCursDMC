4009c708fdf82d705bb0cff7003d9c319e6aae7f
Alin Zamfiroiu <alinz10@yahoo.com>
2024-04-20 13:59:06 +0300

    Grafica bidimensionala

---
+++ app/M01Grafica2D/src/main/java/ro/zamfiroiu/m01grafica2d/MainActivity.java
@@ -0,0 +1,16 @@
+package ro.zamfiroiu.m01grafica2d;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new DesenPentruPaints(this));
+    }
+
+
+}
